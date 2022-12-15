eb438fe8978ff68f4e53da2f6690276106c2e9b6
İpek Karakurt <ipekkarakurt@yandex.com>
2022-12-15 16:47:20 +0300

    Reformat

--- day-2/main.go
+++ day-2/main.go
@@ -1,10 +1,10 @@
 package main
 
-import(
-  "fmt"
-  "os"
-  "bufio"
-  "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 const (
@@ -14,82 +14,82 @@ const (
 )
 
 func main() {
-  f, _ := os.Open("input.txt")
+	f, _ := os.Open("input.txt")
 
-  scanner := bufio.NewScanner(f)
+	scanner := bufio.NewScanner(f)
 
-  var score_1 int
-  var score_2 int
+	var score_1 int
+	var score_2 int
 
-  for scanner.Scan() {
-    parts := strings.Split(scanner.Text(), " ")
-    score_1 = firstPart(parts, score_1)
-    score_2 = secondPart(parts, score_2)
-  }
-  fmt.Println("First part: ", score_1)
-  fmt.Println("Second part: ", score_2)
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), " ")
+		score_1 = firstPart(parts, score_1)
+		score_2 = secondPart(parts, score_2)
+	}
+	fmt.Println("First part: ", score_1)
+	fmt.Println("Second part: ", score_2)
 }
 
-func firstPart(parts []string, score int) int{
-  translateMoves := map[string]string{"A": "Rock", "B": "Paper", "C": "Scissors", "X": "Rock", "Y": "Paper", "Z": "Scissors"}
+func firstPart(parts []string, score int) int {
+	translateMoves := map[string]string{"A": "Rock", "B": "Paper", "C": "Scissors", "X": "Rock", "Y": "Paper", "Z": "Scissors"}
 
-  score += calculateRoundScoreByMoves(translateMoves[parts[0]], translateMoves[parts[1]])
-  return score
+	score += calculateRoundScoreByMoves(translateMoves[parts[0]], translateMoves[parts[1]])
+	return score
 }
 
-func calculateRoundScoreByMoves(opponentMove string, myMove string) int{
-  var roundScore int
-  moveScore := map[string]int {"Rock": 1, "Paper": 2, "Scissors": 3}
+func calculateRoundScoreByMoves(opponentMove string, myMove string) int {
+	var roundScore int
+	moveScore := map[string]int{"Rock": 1, "Paper": 2, "Scissors": 3}
 
-  if opponentMove == myMove {
-    roundScore = Draw + moveScore[myMove]
-    return roundScore
-  } else if myMove == "Rock" && opponentMove == "Paper" {
-    return moveScore[myMove]
-  } else if myMove == "Paper" && opponentMove == "Scissors" {
-    return moveScore[myMove]
-  } else if myMove == "Scissors" && opponentMove == "Rock" {
-    return moveScore[myMove]
-  } else {
-    roundScore = Win + moveScore[myMove]
-    return roundScore
-  }
+	if opponentMove == myMove {
+		roundScore = Draw + moveScore[myMove]
+		return roundScore
+	} else if myMove == "Rock" && opponentMove == "Paper" {
+		return moveScore[myMove]
+	} else if myMove == "Paper" && opponentMove == "Scissors" {
+		return moveScore[myMove]
+	} else if myMove == "Scissors" && opponentMove == "Rock" {
+		return moveScore[myMove]
+	} else {
+		roundScore = Win + moveScore[myMove]
+		return roundScore
+	}
 }
 
-  func secondPart(parts []string, score int) int{
-    translateMoves := map[string]string{"A": "Rock", "B": "Paper", "C": "Scissors"}
-    translateOutcomes := map[string]string{"X": "Lose", "Y": "Draw", "Z": "Win"}
+func secondPart(parts []string, score int) int {
+	translateMoves := map[string]string{"A": "Rock", "B": "Paper", "C": "Scissors"}
+	translateOutcomes := map[string]string{"X": "Lose", "Y": "Draw", "Z": "Win"}
 
-    score += calculateRoundScoreByOutcome(translateMoves[parts[0]], translateOutcomes[parts[1]])
-    return score
-  }
+	score += calculateRoundScoreByOutcome(translateMoves[parts[0]], translateOutcomes[parts[1]])
+	return score
+}
 
-  func calculateRoundScoreByOutcome(opponentMove string, outcome string) int{
-    var myMove string
-    var roundScore int
-    moveScore := map[string]int {"Rock": 1, "Paper": 2, "Scissors": 3}
+func calculateRoundScoreByOutcome(opponentMove string, outcome string) int {
+	var myMove string
+	var roundScore int
+	moveScore := map[string]int{"Rock": 1, "Paper": 2, "Scissors": 3}
 
-    if outcome == "Draw" {
-      myMove = opponentMove
-      roundScore = Draw + moveScore[myMove]
-    } else if outcome == "Lose" {
-      if opponentMove == "Rock" {
-        myMove = "Scissors"
-      } else if opponentMove == "Paper" {
-        myMove = "Rock"
-      } else if opponentMove == "Scissors" {
-        myMove = "Paper"
-      }
-      roundScore = moveScore[myMove]
-    } else {
-      if opponentMove == "Rock" {
-        myMove = "Paper"
-      } else if opponentMove == "Paper" {
-        myMove = "Scissors"
-      } else if opponentMove == "Scissors" {
-        myMove = "Rock"
-      }
-      roundScore = moveScore[myMove] + Win
-    }
-    return roundScore
-  }
+	if outcome == "Draw" {
+		myMove = opponentMove
+		roundScore = Draw + moveScore[myMove]
+	} else if outcome == "Lose" {
+		if opponentMove == "Rock" {
+			myMove = "Scissors"
+		} else if opponentMove == "Paper" {
+			myMove = "Rock"
+		} else if opponentMove == "Scissors" {
+			myMove = "Paper"
+		}
+		roundScore = moveScore[myMove]
+	} else {
+		if opponentMove == "Rock" {
+			myMove = "Paper"
+		} else if opponentMove == "Paper" {
+			myMove = "Scissors"
+		} else if opponentMove == "Scissors" {
+			myMove = "Rock"
+		}
+		roundScore = moveScore[myMove] + Win
+	}
+	return roundScore
+}
